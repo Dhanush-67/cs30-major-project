de859106a68d258e0dbbe0e81c4aef5e064d74a4
Dhanush-67 <7003902@spsd.sk.ca>
2023-12-24 23:39:36 -0600

    Fixed a rotation log bug, worked on a better collision detection system, and added the earth

--- p5play/sketch.js
+++ p5play/sketch.js
@@ -5,103 +5,103 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
-let spaceship;
+//Global variables
 let bgImg;
+
+//Spaceship stuff
+let spaceship;
 let spaceshipImg;
+
+//Asteroid stuff
 let asteroidImg;
-let asteroidArray;
+let asteroidArray = [];
 
 
+//preload
 function preload() {
   bgImg = loadImage("Assets/Images/space bg.png");
   spaceshipImg = loadImage("Assets/Images/Spaceship3.png")
-  //spaceshipImg = loadImage("Assets/Images/turned spaceship.png");
-  asteroidImg = loadImage("Assets/Images/Asteroid image.png");
+  asteroidImg = loadImage("Assets/Images/Asteroid image.png")
 }
 
+
+//Setup
 function setup(){
   new Canvas();
-  createSpaceship(width/2,height/2,50,50);
-  
-  // for (let i = 0; i < 10; i++) {
-  //   asteroidArray.push(createAsteroid());
-  // }
+  createSpaceship(width/2,height/2,spaceshipImg.height-30,60);
+
+  new Sprite(100,300,50,50);
+
+  for (let i = 0; i < 10; i++) {
+    asteroidArray.push(createAsteroid());
+  }
 
 }
 
+//Draw loop
 function draw() {
   clear();
   background(bgImg);
   spaceshipControls();
-  //spaceship.updateMovement();
   edges();
 }
 
-//Spaceship stuff
+//Displays the asteroid
+function createAsteroid(){
+  posX = random(width)
+  posY = random(height)
+  hitbox = asteroidImg.width*0.5-80
+  asteroid = new Sprite(posX,posY,hitbox,hitbox);
+  //asteroid.set collider("circle", 0, 0, 25)
+  asteroid.debug = true;
+  push();
+  asteroid.scale = 0.5;
+  imageMode(CENTER);
+  asteroid.mass = 1
+  asteroid.addImage(asteroidImg);
+  pop();
+}
+
+//Displays the spaceship
 function createSpaceship(x,y,w,h){
   spaceship = new Sprite(x, y, w, h);
   push();
   spaceship.scale = 0.5;
   imageMode(CENTER);
+  spaceship.mass = 5
   spaceship.addImage(spaceshipImg);
+  spaceship.rotationLock = true
+  spaceship.bounce = 5
   pop();
 
 }
-
-
-//   spaceship.updateMovement = function() {
-//     if (kb.pressing('up')){
-//       spaceship.direction = spaceship.rotation - 90;
-      
-
-//       if(spaceship.speed < 10)
-//       spaceship.speed += 0.2;
-//       console.log(spaceship.vel.y);
-//     } 
-//     else{
-//       if (spaceship.speed > 0) {
-//         spaceship.speed *= 0.95;
-//       }
-//     } 
-//     if (kb.pressing('right')) spaceship.rotation += 5;
-//     if (kb.pressing('left')) spaceship.rotation -= 5;
-//   };
-// }
 
 
 // when we press "w" speed gets added to the spacship until its speed reaches 10 after which no more 
 // speed is added to the spaceship.Whatever speed it has gained it will have that speed. When "w" is
 // released then we multiply speed by a decimal number which acts as friction.
 function spaceshipControls(){
+  spaceship.speed *= 0.99;
+  spaceship.debug = true
+
   if (kb.pressing("up")){
-    //spaceship.heading = spaceship.rotation-90;
-    spaceship.bearing = spaceship.rotation;
-    spaceship.applyForce(10)
-    // if(spaceship.speed < 10){
-    //   spaceship.speed += 0.1;
-    //   console.log(spaceship.speed)
-    // }
+    if(spaceship.speed < 10){
+      spaceship.vel.x += cos(spaceship.rotation) * 0.15;
+      spaceship.vel.y += sin(spaceship.rotation) * 0.15;
+    }
   } 
 
 
-  // else{
-  //   if (spaceship.speed > 0) {
-  //     spaceship.speed *= 0.985;
-  //   }
-  // } 
-
   if (kb.pressing("right")){
-
-    spaceship.rotation += 5;
-    //spaceship.speed *= 0.99;
+    spaceship.rotation += 4;
   }
   if (kb.pressing("left")){
-    spaceship.rotation -= 5;
-    //spaceship.speed *= 0.99;
+    spaceship.rotation -= 4;
   } 
 }
 
 
+// if object goes beyond the screen it spawns on the opposite side of the screen
 function edges(){
   if (spaceship.x > width + spaceship.w){
     spaceship.x = -10;
